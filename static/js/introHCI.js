198c305cbcb38ea3dca47a6cfdeb78588a881565
Arshay Rao <rao.arshay@gmail.com>
2021-01-14 12:16:55 -0800

    Added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
     initializePage();
 });
 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+}
+
 function initializePage() {
-    $('#testjs').click(function(e) {
+    $("#testjs").click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
     $("a.thumbnail").click(projectClick);
 }
 
-function projectClick(e){
-	e.preventDefault();
-	$(this).css("background-color", "#7fff00");
-}
